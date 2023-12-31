0802e0c066bf62cf19b5dac7510a420aea77ae85
unknown <ghs.guilherme.silva@gmail.com>
2023-12-30 21:30:09 -0300

    fix column name in script

--- src/main/resources/db/migration/V2__insert-table-users.sql
+++ src/main/resources/db/migration/V2__insert-table-users.sql
@@ -3,5 +3,5 @@
 -- http://localhost:8080/login
 
 -- USER
-INSERT INTO users (email, password, name, authentication_client_type) VALUES
+INSERT INTO users (email, password, name, auth_client_type) VALUES
 ('admin@todolist.com.br','$2a$10$Y50UaMFOxteibQEYLrwuHeehHYfcoafCopUazP12.rqB41bsolF5.', 'Administrator', 'TODOLIST');
