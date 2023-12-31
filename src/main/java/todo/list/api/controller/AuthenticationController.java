19bee699a5a871a926cba1d139b838be45be9128
unknown <ghs.guilherme.silva@gmail.com>
2023-12-31 11:52:08 -0300

    starting the implementation of the oauth2 post login controller callback

--- src/main/java/todo/list/api/controller/AuthenticationController.java
+++ src/main/java/todo/list/api/controller/AuthenticationController.java
@@ -1,21 +1,21 @@
 package todo.list.api.controller;
 
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
-import org.springframework.web.util.UriComponentsBuilder;
-import todo.list.api.domain.user.*;
-import todo.list.api.infra.security.TokenJWT;
-import todo.list.api.infra.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import todo.list.api.domain.user.*;
+import todo.list.api.infra.security.TokenJWT;
+import todo.list.api.infra.security.TokenService;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/")
 public class AuthenticationController {
 
     @Autowired
@@ -27,8 +27,18 @@ public class AuthenticationController {
     @Autowired
     private UserService userService;
 
-    @PostMapping
-    public ResponseEntity signIn(@RequestBody @Valid SignInRequest data) {
+    @GetMapping
+    public void callback(HttpServletResponse httpResponse) {
+        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        //todo: verify, save and return user sso
+        //todo: generate token for this user
+
+//        httpResponse.setHeader(HttpHeaders.SET_COOKIE, "user=" + Base64.getEncoder().encodeToString(tokenJWT.getBytes())+"; Max-Age=86400");
+//        httpResponse.sendRedirect(redirectUrl+ "?token=" + TokenJWT(tokenJWT));
+    }
+
+    @PostMapping("sign-in")
+    public ResponseEntity<TokenJWT> signIn(@RequestBody @Valid SignInRequest data) {
         var authenticationToken = new UsernamePasswordAuthenticationToken(data.email(), data.password());
         var authentication = manager.authenticate(authenticationToken);
         var tokenJWT = tokenService.generateToken((User) authentication.getPrincipal());
@@ -36,8 +46,8 @@ public class AuthenticationController {
         return ResponseEntity.ok(new TokenJWT(tokenJWT));
     }
 
-    @PostMapping("/sign-up")
-    public ResponseEntity signUp(@RequestBody @Valid SignUpRequest data, UriComponentsBuilder uriBuilder) {
+    @PostMapping("sign-up")
+    public ResponseEntity<SignUpResponse> signUp(@RequestBody @Valid SignUpRequest data, UriComponentsBuilder uriBuilder) {
         User user = userService.insertUser(data.email(), data.password(), data.name());
         var uri = uriBuilder.path("/users/{id}").buildAndExpand(user.getId()).toUri();
 
